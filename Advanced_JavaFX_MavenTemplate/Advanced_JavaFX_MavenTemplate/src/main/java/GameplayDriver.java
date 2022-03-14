456ad08d83dd119e081f66f3b3debeb5373d8b8e
mszwoc <100624151+mszwoc@users.noreply.github.com>
2022-03-13 20:47:27 -0500

    Add files via upload

--- Advanced_JavaFX_MavenTemplate/Advanced_JavaFX_MavenTemplate/src/main/java/GameplayDriver.java
+++ Advanced_JavaFX_MavenTemplate/Advanced_JavaFX_MavenTemplate/src/main/java/GameplayDriver.java
@@ -17,8 +17,13 @@ public class GameplayDriver {
 	public static Integer numSpotsSelected; // how many buttons player has chosen currently as winners
 	public static Integer numSpotsTotal;    // how many total buttons a player may choose.
 	public static Integer repeatCards;      // how many times the same card has been used. Max of 4 times usable
+	public static Integer multiplier;
 	
-
+	//
+	//  public static void init_GameplayDriver(Integer cash):
+	//
+	//  initializes datamambers for use in later functions
+    //
 	public static void init_GameplayDriver(Integer cash){
 		setWinnings(cash);
 		setRepeat(0);
@@ -66,8 +71,11 @@ public class GameplayDriver {
 		return numSpotsTotal == numSpotsSelected;
 	}
 	
-	//  This shuffles index's 0 to 80, "randomizing" the contents
+	//  static void shuffleDrawings():
+	//
+	//  This shuffles index's 0 to 79, "randomizing" the contents
 	//  The first 20 index's will be used as winners ( 0- 19 )
+	//
 	static void shuffleDrawings() {
 
 		Random shuffleDraw = new Random();
@@ -84,31 +92,35 @@ public class GameplayDriver {
 		
 		}
 	
+	//  static void autoPicks(int autoPicks):
+	//
 	//  This creates a random generated selection for the player to use
 	//  Will check that the number provided is a valid playable spot number
-	static void autoPicks(int autoPicks) {
-		
+	//
+	static void autoPicks(int autoPicks) {	
 		if (!validNumSpots(autoPicks)) {
 			return;
 		}
 
 		Random shuffleDraw = new Random();
-		 Set<Integer> set = new LinkedHashSet<Integer>();
-		 playerSelections.clear();
-		 while(set.size() < autoPicks) {
-			 set.add(shuffleDraw.nextInt(80) + 1);
+		Set<Integer> set = new LinkedHashSet<Integer>();
+		playerSelections.clear();
+		while(set.size() < autoPicks) {
+			set.add(shuffleDraw.nextInt(80) + 1);
 		 }
  
-		 for(Integer entry : set) {
+		for(Integer entry : set) {
 			incSelected();
 			addNumberToPlayerSelections(entry);
 
-		 }
-		
+		 }	
 		}
 	
+	// public static String findWinners():
+	//
 	// This function finds all of the matches by comparing matches arraylist to 1-19 of drawings
 	// You can access matches by using the string, or just iterate through the matches ArrayList
+	//
 	public static String findWinners() {
 		String result = "";
 		matches.clear();
@@ -120,11 +132,13 @@ public class GameplayDriver {
 			}
 		}
 		return result;
-		
-	
 	}
 	
-	// Extracts drawings[0] - drawings[19] and returns it as a string. Can be used for testing, but leaving it up here in case you decide to use it
+	//  public static String extractSelectionsString():
+	//
+	//  Extracts drawings[0] - drawings[19] and returns it as a string.
+	//  Can be used for testing, but may be used in other ways
+	//
 	public static String extractDrawingsString() {
 		String result = "";
 		for (int i = 0; i < 20; i++) {
@@ -132,7 +146,12 @@ public class GameplayDriver {
 		}
 		return result;
 	}
-	//  // Extracts playerSelections and returns it as a string. Can be used for testing, but leaving it up here in case you decide to use it
+	
+	//  public static String extractSelectionsString():
+	//
+	//  Extracts playerSelections and returns it as a string. 
+	//  Can be used for testing, but may be used in other ways
+	//
 	public static String extractSelectionsString() {
 		String result = "";
 		for (int i = 0; i < playerSelections.size(); i++) {
@@ -141,10 +160,13 @@ public class GameplayDriver {
 		return result;
 	}
 	
+	//  public static void reset():
+	//
 	//  clears matches and playerSelections array lists
 	//  numSpotsSelected = 0;
 	//  numSpotsTotal = 0;    
 	//  repeatCards = 0;
+	//
 	public static void reset() {
 		playerSelections.clear();
 		if (matches.size() > 0) {
@@ -154,9 +176,83 @@ public class GameplayDriver {
 		
 	}
 	
+	//  public static Integer calculateWinnings():
+	
+	//  Based on how many spots you play and match, updates players cash.
+	//  Also returns gross winnings (winnings from this draw, not including player cash)
+	//	
+	public static Integer calculateWinnings(Integer mult) {
+		Integer multiplier = mult;    //Currently there is no options to "double down" or similar. If we add this functionality, this will become much more useful.
+		Integer n = matches.size();
+		Integer winnings = 0;
+		if (n == 0) {
+			return 0;
+		}
+		switch(numSpotsTotal) {
+		case 1:
+			if (n == 1) {
+				winnings = 2;
+			}
+			break;
+			
+		case 4:
+			if (n == 2) {
+				winnings = 1;
+			} else if (n == 3) {
+				winnings = 5;
+				
+			} else if (n == 4) {
+				winnings = 75;
+			}
+			break;
+			
+		case 8:
+			if (n == 4) {
+				winnings = 2;
+			} else if (n == 5) {
+				winnings = 12;
+				
+			} else if (n == 6) {
+				winnings = 50;
+				
+			} else if (n == 7) {
+				winnings = 750;
+				}
+			else if (n == 8) {
+				winnings = 10000;
+				}
+			break;
+			
+		case 10:
+			if (n == 5) {
+				winnings = 2;
+				
+			} else if (n == 6) {
+				winnings = 15;
+				
+			} else if (n == 7) {
+				winnings = 40;
+				}
+			else if (n == 8) {
+				winnings = 450;
+				}
+			else if (n == 9) {
+				winnings = 4250;
+				}
+			else if (n == 10) {
+				winnings = 1000000;
+				}
+			break;	
+		}
+		
+		winnings = winnings * multiplier;
+		totalWinnings += winnings;
+		return winnings;
+	}
 	
 	
-	// Setters and getters ------------------------------------->
+	
+	// Setters/Setters and Helper Functions ------------------------------------->
 	
 	// Add a number to player selected array. Should be used with with Boolean incSelected() to avoid adding too many numbers
 	public static void addNumberToPlayerSelections(Integer i) {
@@ -165,8 +261,8 @@ public class GameplayDriver {
 	
 	
 	// remove a number to player selected array. Should be used with with Boolean decSelected() to avoid adding too many numbers
-	public static void removeNumberToPlayerSelections(Integer i) {
-		playerSelections.remove(i);
+	public static void removeNumberPlayerSelections(Integer i) {
+		playerSelections.remove(playerSelections.get(i));
 	}
 
 	
@@ -232,7 +328,9 @@ public class GameplayDriver {
 		return repeatCards;
 	}
 	
-	// Testing Functions: These are for me. Use them if you need to, but be careful--------------------------------------------------------------
+	
+	
+	//*********** Testing Functions: These are for me. Use them if you need to, but be careful***************
 	
 	//  used to initialize drawings[]
 	//  Also if for some reason drawings does not contain the original numbers 1-80, can be used as a reset
